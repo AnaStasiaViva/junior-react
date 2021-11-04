e6dc335fa6a709f02f7be132a5828643912f724e
AnaStasiaViva <52911733+AnaStasiaViva@users.noreply.github.com>
2021-11-04 20:27:50 +0500

    Update Cart.js

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -12,9 +12,9 @@ import { Line, SingleCartWrap, ModalLayout, TotalSum } from "./Cart.styles";
 class Cart extends React.PureComponent {
   //TOTAL PRICE
   calcTotalPrice = () => {
-    let prices = [];
+    const prices = [];
     this.props.cartItems.map((product) => {
-      let prodQ = product.quantity;
+      const prodQ = product.quantity;
       return product.prices.map((price) => {
         if (price.currency === this.props.selectedCurrency) {
           prices.push(price.amount.toFixed() * prodQ);
